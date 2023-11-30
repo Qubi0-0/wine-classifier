3ed52faf14d465795c05b82d244775c1441b4512
Jakub Zeifert <kuba1102@gmail.com>
2023-11-30 15:25:16 +0000

    Refactor DataHandler to read multiple datasets from a folder

--- DataHandler.py
+++ DataHandler.py
@@ -1,30 +1,38 @@
+import os
 import pandas as pd
 
 class DataHandler:
-    def __init__(self, filepath):
-        self.filepath = filepath
-        self.data = None
-    
+    def __init__(self, folderpath):
+        self.folderpath = folderpath
+        self.datasets = {}
+
     def read_data(self):
         try:
-            self.data = pd.read_csv(self.filepath)
-            null_values = self.check_null_values()
-            if null_values is not None:
-                print("Null values found in the data:")
-                print(null_values)
-            return self.data
+            for filename in os.listdir(self.folderpath):
+                if filename.endswith(".csv"):
+                    filepath = os.path.join(self.folderpath, filename)
+                    dataset_name = os.path.splitext(filename)[0]
+                    self.datasets[dataset_name] = pd.read_csv(filepath)
+                    null_values = self.check_null_values(dataset_name)
+                    if null_values is not None:
+                        print(f"Null values found in {dataset_name}:")
+                        print(null_values)
+            return self.datasets
         except FileNotFoundError:
-            print("File not found.")
+            print("Folder not found.")
             return None
 
-    def check_null_values(self):
-        if self.data is not None:
-            return self.data.isnull().sum()
+    def check_null_values(self, dataset_name):
+        if dataset_name in self.datasets:
+            return self.datasets[dataset_name].isnull().sum()
         else:
             return None
 
     
 if __name__ == "__main__":
-    data_handler = DataHandler("data/winequality-red.csv")
-    data = data_handler.read_data()
-    print(data.head())
+    data_handler = DataHandler("data")
+    datasets = data_handler.read_data()
+    for dataset_name, dataset in datasets.items():
+        print(f"Dataset: {dataset_name}")
+        print(dataset.head())
+        print()
