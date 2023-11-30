1387b449ded0dfbe80f89952218a5cf2d0108874
Jakub Zeifert <kuba1102@gmail.com>
2023-11-30 15:55:11 +0000

    Refactor check_null_values function to print null values for each dataset

--- DataHandler.py
+++ DataHandler.py
@@ -9,20 +9,20 @@ def read_data(folderpath):
                 filepath = os.path.join(folderpath, filename)
                 dataset_name = os.path.splitext(filename)[0]
                 datasets[dataset_name] = pd.read_csv(filepath, delimiter=';')
-                null_values = check_null_values(datasets[dataset_name])
-                if null_values is not None:
-                    print(f"Null values found in {dataset_name}:")
-                    print(null_values)
         return datasets
     except FileNotFoundError:
         print("Folder not found.")
         return None
 
-def check_null_values(dataset):
-    return dataset.isnull().sum()
+def check_null_values(dataset, dataset_name):
+    null_values = dataset.isnull().sum()
+    if null_values is not None:
+        print(f"Null values found in {dataset_name}:")
+        print(null_values) 
 
 if __name__ == "__main__":
     datasets = read_data("data/full")
     for dataset_name, dataset in datasets.items():
+        check_null_values(datasets[dataset_name], dataset_name)
         print(f"Dataset: {dataset_name}")
-        print()
+        print(dataset.head())
